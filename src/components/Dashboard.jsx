81ec6953f7826d625bd52f2e9009d6bebccd6bbb
Gogo602 <gogomarosi61@gmail.com>
2025-05-22 22:45:16 +0300

    File:component/dashboard; adjusted paddings

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -23,7 +23,7 @@ const Dashboard = () => {
                     <button className=' flex items-center px-3 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors duration-200'> <FaPlus className='mr-1'/>Add Assets</button>
                 </div>
                 <div className='flex items-center justify-between py-2'>
-                    <div className='flex items-center p-2 space-x-3 bg-white dark:bg-gray-800 rounded-md shadow-sm'> {/* Added shadow-sm */}
+                    <div className='flex items-center p-1 space-x-1 text-sm bg-white dark:bg-gray-800 rounded-md shadow-sm'> {/* Added shadow-sm */}
                         <button
                             onClick={() => setActiveTab('stock')}
                             className={getButtonClasses('stock')}
@@ -38,12 +38,12 @@ const Dashboard = () => {
                         </button>
                     </div>
                    
-                    <ul className='flex items-center space-x-1 text-blue-50'>
-                        <li className='cursor-pointer px-2 py-1 bg-blue-600 rounded-sm'>1D</li>
-                        <li className='cursor-pointer px-2 py-1 bg-blue-600 rounded-sm'>1W</li>
-                        <li className='cursor-pointer px-2 py-1 bg-blue-600 rounded-sm'>1M</li>
-                        <li className='cursor-pointer px-2 py-1 bg-blue-600 rounded-sm'>1Y</li>
-                        <li className='cursor-pointer px-2 py-1 bg-blue-600 rounded-sm'>ALL</li>
+                    <ul className='flex items-center space-x-1 text-blue-50 text-sm'>
+                        <li className='cursor-pointer px-1 bg-blue-600 rounded-sm'>1D</li>
+                        <li className='cursor-pointer px-1 bg-blue-600 rounded-sm'>1W</li>
+                        <li className='cursor-pointer px-1 bg-blue-600 rounded-sm'>1M</li>
+                        <li className='cursor-pointer px-1 bg-blue-600 rounded-sm'>1Y</li>
+                        <li className='cursor-pointer px-1 bg-blue-600 rounded-sm'>ALL</li>
                     </ul>
                 </div>
             </div>
